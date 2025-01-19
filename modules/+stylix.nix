a56b54748c7f0c46e214eb81b67423ae46aa0585
apoxer <allenallen7731@gmail.com>
2025-01-20 00:31:24 +0530

    kitty and other fonts

--- modules/+stylix.nix
+++ modules/+stylix.nix
@@ -5,5 +5,19 @@
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     image = ../walls/pompeii.png;
+    polarity = "dark";
+
   };
+
+  stylix.fonts = {
+    monospace = {
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "JetBrainsMono NF";
+    };
+    sizes.terminal = 14;
+    serif = config.stylix.fonts.monospace;
+    sansSerif = config.stylix.fonts.monospace;
+    emoji = config.stylix.fonts.monospace;
+  };
+
 }
--- modules/kitty.nix
+++ modules/kitty.nix
@@ -4,6 +4,9 @@
   programs.kitty = {
     enable = true;
     extraConfig = "
+      bold_font JetBrainsMono NF ExtraBold
+      italic_font JetBrainsMono Nerd Font Italic
+      bold_italic_font JetBrainsMono NF ExtraBold Italic
       window_padding_width 7
       confirm_os_window_close 0
     ";
